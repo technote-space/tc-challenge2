d6560286dda3ae8f95f171d21da20332258fb0c4
Technote <technote.space@gmail.com>
2019-05-02 01:53:49 +0900

    python

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,1 @@
-for x in range(1, 101): print("FizzBuzz" if 0 == x % 15 else "Fizz" if 0 == x % 3 else "Buzz" if 0 == x % 5 else x)
+for x in range(1, 151): print(str(x) + ("!" if 0 == x % 3 or 3 == x % 10 else ""))
