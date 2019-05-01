6753e2b2ce5b0887eed0c51fba5900d41cfe06f7
Technote <technote.space@gmail.com>
2019-05-02 01:41:11 +0900

    trivial change

--- C/main.c
+++ C/main.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 
 int main() {
-	for (int i = 1; i <= 150; i++) 0 == i % 3 || 3 == i % 10 ? printf("%d!\n", i) : printf("%d\n", i);
+	for (int i = 1; i <= 150; i++) printf(0 == i % 3 || 3 == i % 10 ? "%d!\n" : "%d\n", i);
 }
