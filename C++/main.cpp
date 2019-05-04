28cff7b1b528dbad67acef81152182bd942cf365
Technote <technote.space@gmail.com>
2019-05-04 15:01:14 +0900

    trivial change

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,5 +2,5 @@
 #include <string>
 
 int main() {
-	for (int i = 1; i <= 150; i++) std::cout << std::to_string(i) << (0 == i % 3 || 3 == i % 10 ? "!" : "") << std::endl;
+	for (int i = 1; i <= 150; i++) std::cout << i << (0 == i % 3 || 3 == i % 10 ? "!" : "") << std::endl;
 }
